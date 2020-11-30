99a912f623f5a71805d7e44b434ee13bb6096501
Oleksandr Likhachov <oleksandrlikhachov@Oleksandrs-MacBook-Pro.local>
2020-11-30 11:39:22 +0200

    renamed string variable in HelloWorld class

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,7 +4,7 @@ import core.basesyntax.model.Cat;
 import core.basesyntax.model.Dog;
 
 public class HelloWorld {
-    private String aa1;
+    private String classStringVariable;
     private int variable;
     private int age;
     private int size;
