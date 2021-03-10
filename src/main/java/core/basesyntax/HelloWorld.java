9c87febee861811ac5e2439507b8589778e46829
Maksim <max.freedman.main@gmail.com>
2021-03-10 15:38:53 +0200

    second try to fix all code style violations

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,33 +1,25 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Cat;
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 
-import core.basesyntax.Model.Dog;
-
-public class Hello_World
-{
-    private String a;
-    private int vARIaBLE;
-    private int age, size;
-
-
+public class HelloWorld {
     public static void main(String[] args) {
         Dog dog = new Dog();
-        Cat cat = new Cat(); }
-
-    private String getFromOneToTen() {
-        String result="";
-
-        for (int i=0; i<10; i++){
-            if(i%2==0)
-             result = result + i + " ";
-        }
-        return result;
+        Cat cat = new Cat();
     }
 
-    private void sayHello(String HELLO) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
-        " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+    private String getFromOneToTen() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                result.append(i).append(" ");
+            }
+        }
+        return result.toString();
+    }
+
+    private void sayHello(String hello) {
+        System.out.println(hello);
     }
 }
