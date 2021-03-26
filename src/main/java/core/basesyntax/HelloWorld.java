655fb94daa04d86e45b1e95522b14a78f5eda284
AventusDev <aventus@ex.ua>
2021-03-26 13:50:22 +0200

    fix problems

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,10 +1,10 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Cat;
-import core.basesyntax.Model.Dog;
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 
 public class HelloWorld {
-    private String a;
+    private String str;
     private int variable;
     private int age;
     private int size;
@@ -17,7 +17,7 @@ public class HelloWorld {
     private String getFromOneToTen() {
         String result = "";
 
-        for (int i = 0; i<10; i++){
+        for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
                 result = result + i + " ";
             }
@@ -26,8 +26,9 @@ public class HelloWorld {
     }
 
     private void sayHello(String hello) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
-        " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! "
+                + "Your task is to fix the checkstyle in this file."
+                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 }
