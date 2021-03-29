00e533fbb42cd19bcb74f11945c8f9d965e79ad8
Anton Haniyev <anton199906@gmail.com>
2021-03-29 09:20:07 +0300

    added solution

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,8 +4,7 @@ import core.basesyntax.model.Cat;
 import core.basesyntax.model.Dog;
 
 public class HelloWorld {
-    private int a5F;
-
+    private String name;
     private int variable;
     private int size;
     private int age;
@@ -13,29 +12,26 @@ public class HelloWorld {
     public static void main(String[] args) {
         Cat cat = new Cat();
         Dog dog = new Dog();
-
     }
 
     private String getFromOneToTen() {
         String result = "";
+
         for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
-                result = result + i + "";
+                result = result + i + " ";
             }
         }
         return result;
     }
 
     private void sayHello(String hello) {
-        System.out.println("Hello, Mate! "
-                           +
-                           "I am happy to see you! "
-                           +
-                           "This class looks weird! "
-                           +
-                           "Your task is to fix the checkstyle in this file."
-                           +
-                " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println("Hello, Mate! I am happy to see you! "
+                + "This class looks weird! "
+
+                + "Your task is to fix the checkstyle in this file."
+
+                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 }
