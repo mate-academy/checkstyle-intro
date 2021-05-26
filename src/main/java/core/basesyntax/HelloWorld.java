e5f420d277f42890a21299172e979b2d1d749f97
“Valerii <torsher09@gmail.com>
2021-05-26 16:30:42 +0300

    changed var date to variable name

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,11 +1,10 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Cat;
-import core.basesyntax.Model.Dog;
-
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
-public class Hello_World {
+public class HelloWorld {
     public static void main(String[] args) {
         Dog dog = new Dog();
         Cat cat = new Cat();
@@ -21,18 +20,18 @@ public class Hello_World {
         return result;
     }
 
-    private void sayHello(String HELLO) {
+    private void sayHello(String hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird!"
                 + " Your task is to fix the checkstyle in this file."
                 + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+        System.out.println(hello);
     }
 
     private void initializeVariables() {
-        String GREETINGS = "Hello mates!";
-        System.out.println(GREETINGS);
-        int DATE = LocalDate.now().getYear();
-        System.out.println("It is " + DATE + "year");
+        String greetings = "Hello mates!";
+        System.out.println(greetings);
+        int variable = LocalDate.now().getYear();
+        System.out.println("It is " + variable + "year");
         int age = 25;
         System.out.println("I'm " + age);
         int size = 42;
