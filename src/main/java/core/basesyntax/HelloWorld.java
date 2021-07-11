a7b56ea2f223638c8a16646833231b19e9a8ba9f
Vadym Vyshnevskyi <wallian169@gmail.com>
2021-07-11 16:02:41 +0300

    added braces, name 'nello' match the pattern

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -18,17 +18,18 @@ public class HelloWorld {
     private String getFromOneToTen() {
         String result = "";
 
-        for (int i = 0; i < 10; i++){
-            if(i % 2 == 0)
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
                 result = result + i + " ";
+            }
         }
         return result;
     }
 
-    private void sayHello(String HELLO) {
+    private void sayHello(String hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
         " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+        System.out.println(hello);
     }
 
     private void initializeVariables() {
