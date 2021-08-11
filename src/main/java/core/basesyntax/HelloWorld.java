6419c542dad3c9392e60ff89839539a83198c3c5
Maxim Moshenskiy <osidiv@ukr.net>
2021-08-11 13:53:00 +0300

    added solution

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -19,16 +19,16 @@ public class HelloWorld {
         String result = "";
         for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
-                result = result + i + " ";
+                result += i + " ";
             }
         }
         return result;
     }
 
     private void sayHello(String hello) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird!" +
-                " Your task is to fix the checkstyle in this file." +
-                " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! "
+                + "Your task is to fix the checkstyle in this file. "
+                + "Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 
