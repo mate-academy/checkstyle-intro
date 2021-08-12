e78c64b41d7a6f9a90e2a628a0fc20a1e146dc39
Taras Oleksiuk <64101459+taras-oleksiuk@users.noreply.github.com>
2021-08-12 15:15:12 +0300

    Update HelloWorld.java

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,7 +5,7 @@ import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-    private String greeting = "";
+    private String greeting;
     private int variable;
     private int age;
     private int size;
