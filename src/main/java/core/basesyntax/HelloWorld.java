69e02e6cb91611634b0a294915bf6a7de0a14be6
Taras Oleksiuk <64101459+taras-oleksiuk@users.noreply.github.com>
2021-08-12 15:30:49 +0300

    Update HelloWorld.java

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -28,10 +28,8 @@ public class HelloWorld {
 
     private void sayHello(String hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! You"
-                           + "r task is to fix the  Your task is to fix the checkstyle in this 
-                           fil"
-                           + "e. Your goal is to run `mvn clean package` and receive `BUILD 
-                           SUCCESS` message");
+                           + "r task is to fix the  Your task is to fix the checkstyle in this fil"
+                           + "e. Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 
