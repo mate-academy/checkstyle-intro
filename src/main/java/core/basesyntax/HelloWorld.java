d390f778a6d0824504cf03a6715765291202ce10
vitalii_yelinskyi <yelinskyizp@gmail.com>
2021-08-12 13:29:11 +0300

    correct name 'variable' to 'greeting', 'name' to 'year'

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,8 +5,8 @@ import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-    private String name;
-    private int variable;
+    private String greeting;
+    private int year;
     private int age;
     private int size;
 
@@ -35,10 +35,10 @@ public class HelloWorld {
     }
 
     private void initializeVariables() {
-        name = "Hello mates!";
-        System.out.println(name);
-        variable = LocalDate.now().getYear();
-        System.out.println("It is " + variable + "year");
+        greeting = "Hello mates!";
+        System.out.println(greeting);
+        year = LocalDate.now().getYear();
+        System.out.println("It is " + year + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
