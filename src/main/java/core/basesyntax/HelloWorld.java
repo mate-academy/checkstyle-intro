c1695bba092bc7ad55c8ff1436c6029363dc1553
SolnalarmJulia <solnalarm@meta.ua>
2021-08-12 14:49:39 +0300

    moved file HelloWorld from model to basesyntax package; rename String field to greeting; fixed toString() in Cat;

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,10 +1,12 @@
-package core.basesyntax.model;
+package core.basesyntax;
 
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-    private String memberName;
-    private int variable;
+    private String greeting;
+    private int year;
     private int age;
     private int size;
 
@@ -33,10 +35,10 @@ public class HelloWorld {
     }
 
     private void initializeVariables() {
-        memberName = "Hello mates!";
-        System.out.println(memberName);
-        variable = LocalDate.now().getYear();
-        System.out.println("It is " + variable + "year");
+        greeting = "Hello mates!";
+        System.out.println(greeting);
+        year = LocalDate.now().getYear();
+        System.out.println("It is " + year + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
