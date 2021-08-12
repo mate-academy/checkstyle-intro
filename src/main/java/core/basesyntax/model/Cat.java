cea31022cf6e993e507829b86f22f02e37529ddd
Serhii Orlov <orlov30@gmail.com>
2021-08-12 10:56:04 +0300

    Fix problem: rename variable, delete local variable (a) and move "age"

--- src/main/java/core/basesyntax/model/Cat.java
+++ src/main/java/core/basesyntax/model/Cat.java
@@ -8,8 +8,8 @@ public class Cat {
     @Override
     public String toString() {
         return "Cat{"
-                + "age="
-                + age + ", name='" + name + '\''
+                + "age=" + age
+                + ", name='" + name + '\''
                 + ", ownerName='" + ownerName
                 + '\'' + '}';
     }
