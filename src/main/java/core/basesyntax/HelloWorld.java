4aca0aae209fcb2860b2ee35ab8cac521cf8a1f8
Roman Legendzevych <86983401+Black3942@users.noreply.github.com>
2021-08-12 17:27:26 +0300

    all comments was removed
    all comments was removed and messeges variable was changed

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -3,11 +3,10 @@ package core.basesyntax;
 import core.basesyntax.model.Cat;
 import core.basesyntax.model.Dog;
 import java.time.LocalDate;
-// import java.util.Objects;
 
 public class HelloWorld {
     private String message;
-    private int variable;
+    private int messageVariable;
     private int age;
     private int size;
 
@@ -28,23 +27,21 @@ public class HelloWorld {
     }
 
     private void sayHello(String hello) {
-        StringBuilder mes = new StringBuilder();
         String message = "Hello, Mate! "
         		+ "I am happy to see you! ";
         		+ "This class looks weird! ";
         		+ "Your task is to fix the checkstyle in this file.";
-        System.out.println(me1 + me2 + me3 + me4);
-        String mes2;
-        mes2 = "Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message";
-        System.out.println(mes2);
+        String message2;
+        message2 = "Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message";
+        System.out.println(message2);
         System.out.println(hello);
     }
 
     private void initializeVariables() {
         message = "Hello mates!";
         System.out.println(message);
-        variable = LocalDate.now().getYear();
-        System.out.println("It is " + variable + "year");
+        messageVariable = LocalDate.now().getYear();
+        System.out.println("It is " + messageVariable + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
