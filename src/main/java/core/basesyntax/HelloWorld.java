820cbd47b6c57e1ba10316c3c7bf134e0c370fdb
Denys Smei <denys.smei@artcodevision.com>
2021-08-12 13:16:51 +0300

    All checks OK

---
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -0,0 +1,48 @@
+package core.basesyntax;
+
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
+import java.time.LocalDate;
+
+public class HelloWorld {
+    private String textOut;
+    private int actualYear;
+    private int age;
+    private int size;
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+    }
+
+    private String getFromOneToTen() {
+        String result = "";
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                result = result + i + " ";
+            }
+        }
+        return result;
+    }
+
+    private void sayHello(String hello) {
+        StringBuilder textWeWantToDisplay = new StringBuilder();
+        textWeWantToDisplay.append("Hello, Mate! I am happy to see you! This class looks weird! ");
+        textWeWantToDisplay.append("Your task is to fix the checkstyle in this file.");
+        textWeWantToDisplay.append("Your goal is to run `mvn clean package`");
+        textWeWantToDisplay.append(" and receive `BUILD SUCCESS` message");
+        System.out.println(textWeWantToDisplay);
+        System.out.println(hello);
+    }
+
+    private void initializeVariables() {
+        textOut = "Hello mates!";
+        System.out.println(textOut);
+        actualYear = LocalDate.now().getYear();
+        System.out.println("It is " + actualYear + "year");
+        age = 25;
+        System.out.println("I'm " + age);
+        size = 42;
+        System.out.println("The size is " + size);
+    }
+}
