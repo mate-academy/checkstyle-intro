3828780a947e11585be686365c48307c1ae03652
SonyaPakhomova <88778908+SonyaPakhomova@users.noreply.github.com>
2021-08-13 15:55:14 +0300

    changes 3

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,7 +5,7 @@ import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-    private String welcome;
+    private String greeting;
     private int year;
     private int age;
     private int size;
@@ -34,8 +34,8 @@ public class HelloWorld {
     }
 
     private void initializeVariables() {
-        welcome = "Hello mates!";
-        System.out.println(welcome);
+        greeting = "Hello mates!";
+        System.out.println(greeting);
         year = LocalDate.now().getYear();
         System.out.println("It is " + year + "year");
         age = 25;
