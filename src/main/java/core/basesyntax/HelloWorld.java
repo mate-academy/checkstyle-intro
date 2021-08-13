07fcda48df4c7a1deabdbca6c263fd059c3e9c65
Viktor Yukhno <pingvinomania@gmail.com>
2021-08-13 14:29:54 +0300

    added fix names of variable

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,8 +5,8 @@ import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-    private String welcome;
-    private int variable;
+    private String message;
+    private int currentYear;
     private int age;
     private int size;
 
@@ -34,10 +34,10 @@ public class HelloWorld {
     }
 
     private void initializeVariables() {
-        welcome = "Hello mates!";
-        System.out.println(welcome);
-        variable = LocalDate.now().getYear();
-        System.out.println("It is " + variable + "year");
+        message = "Hello mates!";
+        System.out.println(message);
+        currentYear = LocalDate.now().getYear();
+        System.out.println("It is " + currentYear + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
