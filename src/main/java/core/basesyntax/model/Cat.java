4df347f2923592670e394c3b2fb06705abdede75
Stas Shepet <milka33349@gmail.com>
2021-08-13 11:59:41 +0300

    package names was changed

--- src/main/java/core/basesyntax/model/Cat.java
+++ src/main/java/core/basesyntax/model/Cat.java
@@ -7,7 +7,10 @@ public class Cat {
 
     @Override
     public String toString() {
-        return "Cat{" + "age=" + age + ", name='" + name + '\'' + ", ownerName='" + ownerName
-                + '\'' + '}';
+        return "Cat{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", ownerName='" + ownerName + '\'' +
+                '}';
     }
 }
