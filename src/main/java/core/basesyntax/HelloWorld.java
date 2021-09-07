ef38d3e0bbc8e62b6558fc4291bf8bd385a03757
Vladyslav Chyver <lione2009lione@gmail.con>
2021-09-07 22:19:04 +0300

    added fix of mistakes

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,12 +1,11 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Dog;
-import core.basesyntax.Model.Cat;
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
-public class Hello_World
-{
-    private String a;
+class HelloWorld {
+    private String str;
     private int variable;
     private int age;
     private int size;
@@ -19,29 +18,29 @@ public class Hello_World
     private String getFromOneToTen() {
         String result = "";
 
-        for (int i = 0; i < 10; i++){
-            if(i % 2 == 0){
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
                 result = result + i + " ";
             }
         }
         return result;
     }
 
-    private void sayHello(String HELLO) {
-        System.out.println("Hello, Mate! I am happy to see you! " +
-                "This class looks weird! Your task is to fix the checkstyle in this file." +
-                " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+    private void sayHello(String hello) {
+        System.out.println("Hello, Mate! I am happy to see you! "
+                + "This class looks weird! Your task is to fix the checkstyle in this file."
+                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println(hello);
     }
 
     private void initializeVariables() {
-        a = "Hello mates!";
-        System.out.println(a);
+        str = "Hello mates!";
+        System.out.println(str);
         variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
         System.out.println("The size is " + size);
-     }
+    }
 }
