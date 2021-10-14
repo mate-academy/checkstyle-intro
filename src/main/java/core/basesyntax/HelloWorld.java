7499454251d1d6fbd387d459f01e8c2ecd9f6389
Andrey Sidorkin <andrei.sidorkin@gmail.com>
2021-10-14 19:04:48 +0300

    All the maven errors are fixed

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,12 +1,11 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Cat;
-import core.basesyntax.Model.Dog;
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-
-    private String a;
+    private String greeting;
     private int variable;
     private int age;
     private int size;
@@ -14,27 +13,28 @@ public class HelloWorld {
     public static void main(String[] args) {
         Cat cat = new Cat();
         Dog dog = new Dog();
-        }
+    }
 
     private String getFromOneToTen() {
         String result = "";
         for (int i = 0; i < 10; i++) {
-               if (i % 2 == 0) {
-               result += i + " ";
+            if (i % 2 == 0) {
+                result += i + " ";
+            }
         }
         return result;
     }
 
     private void sayHello(String hello) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! " +
-                "Your task is to fix the checkstyle in this file."
-              + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! "
+                + "Your task is to fix the checkstyle in this file."
+                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 
     private void initializeVariables() {
-        a = "Hello mates!";
-        System.out.println(a);
+        greeting = "Hello mates!";
+        System.out.println(greeting);
         variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + "year");
         age = 25;
