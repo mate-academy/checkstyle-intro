8a3bee63841cc6936d08ab491226612c6391ce9c
Dany Nesterov <nesterov1200997@gmail.com>
2021-10-14 11:19:45 +0200

    fixed checkstyle mistakes

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -33,8 +33,8 @@ public class HelloWorld {
     }
 
     private void initializeVariables() {
-        String str = "Hello mates!";
-        System.out.println(str);
+        String greeting = "Hello mates!";
+        System.out.println(greeting);
         int currentYear = LocalDate.now().getYear();
         System.out.println("It is " + currentYear + "year");
         int age = 25;
