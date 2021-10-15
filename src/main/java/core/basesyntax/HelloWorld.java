29b70bfe76e7d25c8f57139f343ce7363ef8bf25
Dzmitry Kozel <dmitrokoz88@gmail.com>
2021-10-15 17:26:00 +0300

    "variable" renamed to "year"

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 
 public class HelloWorld {
     private String helloMates;
-    private int variable;
+    private int year;
     private int age;
     private int size;
 
@@ -36,8 +36,8 @@ public class HelloWorld {
     private void initializeVariables() {
         helloMates = "Hello mates!";
         System.out.println(helloMates);
-        variable = LocalDate.now().getYear();
-        System.out.println("It is " + variable + "year");
+        year = LocalDate.now().getYear();
+        System.out.println("It is " + year + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
