9b6dd5528ea9ea280bf19431f98056726b0ba884
Svitlana <wsv3ta@gmail.com>
2021-10-18 09:35:58 +0300

    some variables are more informative now and rows was separated more convenient

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,8 +5,8 @@ import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-    private String string;
-    private int variableS;
+    private String greeting;
+    private int year;
     private int age;
     private int size;
 
@@ -34,10 +34,10 @@ public class HelloWorld {
     }
 
     private void initializeVariables() {
-        string = "Hello mates!";
-        System.out.println(string);
-        variableS = LocalDate.now().getYear();
-        System.out.println("It is " + variableS + "year");
+        greeting = "Hello mates!";
+        System.out.println(greeting);
+        year = LocalDate.now().getYear();
+        System.out.println("It is " + year + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
