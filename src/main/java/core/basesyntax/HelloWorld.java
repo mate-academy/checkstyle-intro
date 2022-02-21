f894222c5c1a21998289e6c0bbbb164e15cbd3f0
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2022-02-22 00:25:01 +0200

    Convert instance variables to local

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,10 +5,6 @@ import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-    private String helloMates;
-    private int variable;
-    private int age;
-    private int size;
 
     public static void main(String[] args) {
         Dog dog = new Dog();
@@ -34,13 +30,13 @@ public class HelloWorld {
     }
 
     private void initializeVariables() {
-        helloMates = "Hello mates!";
+        String helloMates = "Hello mates!";
         System.out.println(helloMates);
-        variable = LocalDate.now().getYear();
+        int variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + "year");
-        age = 25;
+        int age = 25;
         System.out.println("I'm " + age);
-        size = 42;
+        int size = 42;
         System.out.println("The size is " + size);
     }
 }
