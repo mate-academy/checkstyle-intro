ab9b6c6c93f5640227f0855090172ef1091f51dd
Yaroslav Lapovets <lapovetsyaroslav@gmail.com>
2022-03-18 01:03:09 +0200

    Added new fix of checkstyle mistakes

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -27,7 +27,7 @@ public class HelloWorld {
     }
 
     private void sayHello(String hello) {
-        System.out.println("Hello, Mate! I am happy to see you This class looks weird!"
+        System.out.println("Hello, Mate! I am happy to see youfd This class looks weird!"
                 + "Your task is to fix the checkstyle in this file."
                 + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
