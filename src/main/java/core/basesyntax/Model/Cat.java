f4288dd305084220e1bdbaa2b0634a86fd2a3b94
Nikita Semisoshenko <jaderussell197@gmail.com>
2022-05-09 04:45:55 +0300

    fixed spaces and line-wrapping mistakes

--- src/main/java/core/basesyntax/Model/Cat.java
+++ src/main/java/core/basesyntax/Model/Cat.java
@@ -7,10 +7,16 @@ public class Cat {
 
     @Override
     public String toString() {
-        return "Cat{" +
-                "age=" + age +
-                ", name='" + name + '\'' +
-                ", ownerName='" + ownerName + '\'' +
+        return "Cat{"
+                + "age="
+                + age
+                + ", name='"
+                + name
+                + '\''
+                + ", ownerName='"
+                + ownerName
+                + '\''
+                +
                 '}';
     }
 }
