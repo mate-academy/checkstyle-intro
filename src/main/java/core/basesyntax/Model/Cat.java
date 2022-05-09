c18abbbdd94a2cf4e876d62340ed63eaf5693e57
Ruslan Matiash <r7714007@gmail.com>
2022-05-09 18:25:11 +0300

    added solution

--- src/main/java/core/basesyntax/Model/Cat.java
+++ src/main/java/core/basesyntax/Model/Cat.java
@@ -1,16 +1,23 @@
-package core.basesyntax.Model;
+package core.basesyntax.model;
 
 public class Cat {
+
     private int age;
     private String name;
     private String ownerName;
 
     @Override
     public String toString() {
-        return "Cat{" +
-                "age=" + age +
-                ", name='" + name + '\'' +
-                ", ownerName='" + ownerName + '\'' +
-                '}';
+        return "Cat{"
+                +
+                "age=" + age
+                +
+                ", name='" + name
+                + '\''
+                +
+                ", ownerName='"
+                + ownerName
+                + '\''
+                + '}';
     }
 }
