dc79babf420a125267fe767fc99041d3c7066263
Mikhail Maiorov <majorov.michail@gmail.com>
2022-06-17 19:40:57 +0300

    fixed checkstyle mistakes

---
+++ src/main/java/core/basesyntax/model/Cat.java
@@ -0,0 +1,16 @@
+package core.basesyntax.model;
+
+public class Cat {
+    private int age;
+    private String name;
+    private String ownerName;
+
+    @Override
+    public String toString() {
+        return "Cat{"
+                + "age=" + this.age
+                + ", name='" + this.name + '\''
+                + ", ownerName='" + this.ownerName + '\''
+                + '}';
+    }
+}
