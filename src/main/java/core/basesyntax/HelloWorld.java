0468b62fec1934495a92466858629c124dd18937
Ilja Nekrituk <Nekrit99@gmail.com>
2022-07-24 12:06:15 +0300

    fixed checkstyle mistakes 4

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,16 +4,16 @@ import core.basesyntax.model.Cat;
 import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
-public class HelloWorld{
+public class HelloWorld {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Dog dog = new Dog();
         Cat cat = new Cat();
     }
 
-    private String getFromOneToTen(){
+    private String getFromOneToTen() {
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
                 result.append(i).append(" ");
             }
@@ -21,14 +21,14 @@ public class HelloWorld{
         return result.toString();
     }
 
-    private void sayHello(String hello){
+    private void sayHello(String hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! "
                 + "Your task is to fix the checkstyle in this file."
                 + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 
-    private void initializeVariables(){
+    private void initializeVariables() {
         String a = "Hello mates!";
         System.out.println(a);
         int variable = LocalDate.now().getYear();
