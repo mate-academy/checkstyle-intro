e4d4d8bf3830e2d9cd0433197b765d19e32588b6
Veers <raadch3011@gmail.com>
2022-07-26 20:05:57 +0300

    fixed all code style violations

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -16,10 +16,9 @@ public class HelloWorld {
 
         for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
-                result = result + i + " ";
+                result += i + " ";
             }
         }
-
         return result;
     }
 
--- src/main/java/core/basesyntax/model/Cat.java
+++ src/main/java/core/basesyntax/model/Cat.java
@@ -14,5 +14,4 @@ public class Cat {
                 + ", ownerName='" + ownerName + '\''
                 + '}';
     }
-
 }
