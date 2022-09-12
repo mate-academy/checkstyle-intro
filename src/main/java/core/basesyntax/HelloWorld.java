978b6ee3a7060c44be32e8f2165e1b82abeff74d
Zoreslav Todorenko <wzxzzx@gmail.com>
2022-09-12 17:29:54 +0300

    style bugs fixed

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,7 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Cat;
-import core.basesyntax.Model.Dog;
-
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
@@ -23,25 +22,26 @@ public class HelloWorld {
             if (i % 2 == 0) {
                 result = result + i + " ";
             }
-            return result;
         }
+        return result;
+    }
 
-        private void sayHello(String hello){
-            System.out.println("Hello, Mate! I am happy to see you! This class looks weird! "
-                    + "Your task is to fix the checkstyle in this file."
-                    + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-            System.out.println(hello);
-        }
+    private void sayHello(String hello) {
+        System.out.println("Hello, Mate! I am happy to see you!"
+                + " This class looks weird!"
+                + " Your task is to fix the checkstyle in this file."
+                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println(hello);
+    }
 
-        private void initializeVariables () {
-            helloMates = "Hello mates!";
-            System.out.println(helloMates);
-            variable = LocalDate.now().getYear();
-            System.out.println("It is " + variable + "year");
-            age = 25;
-            System.out.println("I'm " + age);
-            size = 42;
-            System.out.println("The size is " + size);
-        }
+    private void initializeVariables() {
+        helloMates = "Hello mates!";
+        System.out.println(helloMates);
+        variable = LocalDate.now().getYear();
+        System.out.println("It is " + variable + "year");
+        age = 25;
+        System.out.println("I'm " + age);
+        size = 42;
+        System.out.println("The size is " + size);
     }
 }
