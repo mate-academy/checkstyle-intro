aa67f30668ec3bb2cf092fe43e510f0d24f059b9
Volodymyr Mykychak <leadervlo.d@gmail.com>
2022-09-15 11:11:44 +0300

    added extends to the classfirst HelloWorld

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,10 +1,8 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Dog;
 import core.basesyntax.Model.Cat;
-
+import core.basesyntax.Model.Dog;
 import java.time.LocalDate;
-import java.util.Objects;
 
 public class Hello_World
 {
