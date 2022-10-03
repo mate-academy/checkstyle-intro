73b6aee4d3ff2f6ce550efe693ce93c79de7a4f9
Gleb Shevchenko <glieb.shevchenko@gmail.com>
2022-10-03 13:51:49 +0300

    Initial commit

---
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -0,0 +1,43 @@
+package core.basesyntax;
+
+import core.basesyntax.Model.Dog;
+import core.basesyntax.Model.Cat;
+
+import java.time.LocalDate;
+/*import java.util.Objects;*/
+
+public class HelloWorld {
+    private String A;
+    private int variable;
+    private int age;
+    private int size;
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+    }
+    private String getFromOneToTen() {
+        String result = "";
+        for (int i = 0; i < 10; i++){
+            if (i % 2 == 0) {
+                result += i + " ";
+            }
+        } return result;
+    }
+    private void sayHello(String Hello) {
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! "
+                + "Your task is to fix the checkstyle in this file."
+                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println(Hello);
+    }
+
+        private void initializeVariables() {
+        A = "Hello mates!";
+        System.out.println(A);
+        variable = LocalDate.now().getYear();
+        System.out.println("It is " + variable + "year");
+        age = 25;
+        System.out.println("I'm " + age);
+        size = 42;
+        System.out.println("The size is " + size);
+    }
+}
