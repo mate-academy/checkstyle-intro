6e68bf96a2b1b0677da73b8eafb4d857ce01766b
Bohdan Halitskyi <b.a.galitskiy@gmail.com>
2022-10-13 18:42:32 +0300

    Checkstyle changes for main method

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -6,38 +6,39 @@ import core.basesyntax.Model.Cat;
 import java.time.LocalDate;
 import java.util.Objects;
 
-public class Hello_World
+public class HelloWorld
 {
     private String a;
-    private int vARIaBLE;
-    private int age, size;
+    private int variable;
+    private int age;
+    private int size;
 
 
     public static void main(String[] args) {
         Dog dog = new Dog();
         Cat cat = new Cat(); }
 
-    private String getFromOneToTen() {
-        String result="";
+    private String getFromOneToTen () {
+        StringBuilder result= new StringBuilder();
 
-        for (int i=0; i<10; i++){
-            if(i%2==0)
-             result = result + i + " ";
+        for (int i = 0; i < 10; i++ ){
+            if(i % 2 == 0)
+             result.append(i).append(" ");
         }
-        return result;
+        return result.toString();
     }
 
-    private void sayHello(String HELLO) {
+    private void sayHello(String hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
         " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+        System.out.println(hello);
     }
 
     private void initializeVariables() {
         a = "Hello mates!";
         System.out.println(a);
-        vARIaBLE = LocalDate.now().getYear();
-        System.out.println("It is " + vARIaBLE + "year");
+        variable = LocalDate.now().getYear();
+        System.out.println("It is " + variable + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
