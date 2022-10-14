bdb4bad5f14a3108e1e57ae227c98b3a9e1f8f32
Bohdan Halitskyi <114599463+Bohdan-Halitskyi@users.noreply.github.com>
2022-10-14 16:27:09 +0300

    Update HelloWorld.java

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -18,11 +18,12 @@ public class HelloWorld {
     private String getFromOneToTen () {
         StringBuilder result= new StringBuilder();
 
-        for (int i= 0; i < 10; i++ ){
+        for (int i= 0; i < 10; i++ ) {
             if (i % 2 == 0) {
-             result.append(i).append(" ");
+                result.append(i).append(" ");
+            }
+            return result.toString();
         }
-        return result.toString();
 
     private void sayHello (String hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird!"
@@ -40,5 +41,6 @@ public class HelloWorld {
         System.out.println("I'm " + age);
         size = 42;
         System.out.println("The size is " + size);
-    }
+        }
+
 }
