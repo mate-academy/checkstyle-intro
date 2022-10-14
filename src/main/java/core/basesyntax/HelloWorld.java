544a4106cd05b85959350ed7ce09232ab1430f41
Bohdan Halitskyi <b.a.galitskiy@gmail.com>
2022-10-14 16:39:30 +0300

    Update HelloWorld.java

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -16,15 +16,15 @@ public class HelloWorld {
     }
 
     private String getFromOneToTen () {
-        StringBuilder result= new StringBuilder();
+        StringBuilder result = new StringBuilder();
 
-        for (int i= 0; i < 10; i++ ) {
+        for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
                 result.append(i).append(" ");
             }
             return result.toString();
         }
-
+    }
     private void sayHello (String hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird!"
         + "Your task is to fix the checkstyle in this file." 
