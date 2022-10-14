cf619bf7e3ec0137eb04a09fff85fd80b06c1966
Bohdan Halitskyi <b.a.galitskiy@gmail.com>
2022-10-14 17:04:23 +0300

    Checkstyle edits in several lines

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,13 +5,11 @@ import core.basesyntax.Model.Dog;
 
 import java.time.LocalDate;
 
-public class HelloWorld
-{
-    private String a;
+public class HelloWorld {
+    private String string;
     private int variable;
     private int age;
     private int size;
-
 
     public static void main(String[] args) {
         Dog dog = new Dog();
@@ -19,25 +17,25 @@ public class HelloWorld
     }
 
     private String getFromOneToTen() {
-        String result="";
-
-        for (int i= 0; i<10; i++){
-            if (i% 2 == 0)
+        String result = "";
+        for (int i = 0; i < 10; i++) {
+               if (i % 2 == 0) {
              result = result + i + " ";
+               }
         }
         return result;
     }
 
-    private void sayHello(String Hello) {
+    private void sayHello(String hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird!"
                 + "Your task is to fix the checkstyle in this file."
                 + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(Hello);
+        System.out.println(hello);
     }
 
     private void initializeVariables() {
-        a = "Hello mates!";
-        System.out.println(a);
+        string = "Hello mates!";
+        System.out.println(string);
 
         variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + "year");
