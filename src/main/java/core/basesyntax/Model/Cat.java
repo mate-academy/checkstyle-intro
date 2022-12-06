6d710c3e36df3547ae30a69ddb3c2cb974ff03a9
NGUENA ZEBS <nguenahaupur@gmail.com>
2022-12-06 14:40:28 +0100

    Update Cat.java

--- src/main/java/core/basesyntax/Model/Cat.java
+++ src/main/java/core/basesyntax/Model/Cat.java
@@ -1,16 +1,47 @@
 package core.basesyntax.Model;
 
 public class Cat {
-    private int age;
-    private String name;
-    private String ownerName;
+    
+        private int age;
+	    private String name;
+	    private String ownerName;
+	    
+		public chat() {
+			
+		}
 
-    @Override
-    public String toString() {
-        return "Cat{" +
-                "age=" + age +
-                ", name='" + name + '\'' +
-                ", ownerName='" + ownerName + '\'' +
-                '}';
-    }
+		public chat(int age, String name, String ownerName) {
+			this.age = age;
+			this.name = name;
+			this.ownerName = ownerName;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getOwnerName() {
+			return ownerName;
+		}
+
+		public void setOwnerName(String ownerName) {
+			this.ownerName = ownerName;
+		}
+
+		@Override
+		public String toString() {
+			return "chat [age=" + age + ", name=" + name + ", ownerName=" + ownerName + "]";
+		}
 }
