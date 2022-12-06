1aa69e3afe1301dbdcf780adc444a0fe91c348fe
Olexiy <korm2909@gmail.com>
2022-12-06 20:08:43 +0200

    defenetely ok

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -17,6 +17,7 @@ public class HelloWorld {
 
     private String getFromOneToTen() {
         String result = "";
+
         for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
                 result = result + i + " ";
