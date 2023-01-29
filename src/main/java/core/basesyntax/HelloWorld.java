71b233f9686d0671300d3b004f827a33ac2f0d95
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-01-29 11:36:15 +0200

    Fixed checkstyle in project

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,7 +5,7 @@ import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-    private String message;
+    private String note;
     private int variable;
     private int age;
     private int size;
@@ -34,8 +34,8 @@ public class HelloWorld {
     }
 
     private void initializeVariables() {
-        message = "Hello mates!";
-        System.out.println(message);
+        note = "Hello mates!";
+        System.out.println(note);
         variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + " year ");
         age = 25;
