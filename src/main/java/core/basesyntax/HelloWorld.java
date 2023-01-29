d4b5d46f863880d9d4cc9c7ce8984d72f607bf54
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-01-29 11:50:18 +0200

    changed note on message

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,7 +5,7 @@ import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-    private String note;
+    private String message;
     private int variable;
     private int age;
     private int size;
@@ -34,8 +34,8 @@ public class HelloWorld {
     }
 
     private void initializeVariables() {
-        note = "Hello mates!";
-        System.out.println(note);
+        message = "Hello mates!";
+        System.out.println(message);
         variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + " year ");
         age = 25;
