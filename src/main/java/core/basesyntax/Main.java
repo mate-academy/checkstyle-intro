25d9337553f979a0ecebeef3fa0c7babe1003ec9
Maksym <maxim.yashyn@gmail.com>
2023-02-01 12:53:53 +0200

    removed empty lines

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -11,22 +11,16 @@ public class Main {
     private int variable;
 
     public static void main(String[] args) {
-
         Dog dog = new Dog();
         Cat cat = new Cat();
-
     }
 
     private String getFromOneToTen() {
         String result = "";
         for (int i = 0; i < 10; i++) {
-
             if (i % 2 == 0) {
-
                 result += i + " ";
-
             }
-
         }
         return result;
     }
@@ -38,7 +32,6 @@ public class Main {
     }
 
     private void initializeVariables() {
-
         string = "Hello mates!";
         variable = LocalDate.now().getYear();
         age = 25;
@@ -48,6 +41,5 @@ public class Main {
                 + "It is" + " " + variable + " " + "year" + "\n "
                 + "I'm" + " " + age + "\n "
                 + "The size is" + " " + size);
-
     }
 }
