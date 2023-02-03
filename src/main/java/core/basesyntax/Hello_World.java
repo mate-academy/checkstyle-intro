1b81898e51e80d36bcddb0ef80abbac4e831d273
Danilo Kozakov <tosterworld@gmail.com>
2023-02-03 17:24:48 +0200

    Final (maybe) changes, fix import Hello_World

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -1,14 +1,12 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Dog;
-import core.basesyntax.Model.Cat;
-
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
-import java.util.Objects;
 
 public class Hello_World
 {
-    private String a;
+    private String mates;
     private int variable;
     private int age;
     private int size;
@@ -26,19 +24,19 @@ public class Hello_World
                 result += i + " ";
             }
         }
-
         return result;
     }
 
     private void sayHello(String hello) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file."
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird!"
+               + "Your task is to fix the checkstyle in this file."
                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 
     private void initializeVariables() {
-        a = "Hello mates!";
-        System.out.println(a);
+        mates = "Hello mates!";
+        System.out.println(mates);
         variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + "year");
         age = 25;
