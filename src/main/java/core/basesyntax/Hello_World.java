4f70382c9c80989cdc54bada01d9956784c0ac74
Igor Maksimov <maksigors@gmail.com>
2023-04-13 13:15:21 +0300

    fixed checkstyle mistakes

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -4,40 +4,43 @@ import core.basesyntax.Model.Dog;
 import core.basesyntax.Model.Cat;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 public class Hello_World
 {
-    private String a;
-    private int vARIaBLE;
-    private int age, size;
+    private String message;
+    private int year;
+    private int age;
+    private int size;
 
 
     public static void main(String[] args) {
         Dog dog = new Dog();
-        Cat cat = new Cat(); }
+        Cat cat = new Cat();
+    }
 
     private String getFromOneToTen() {
-        String result="";
+        String result = "";
 
-        for (int i=0; i<10; i++){
-            if(i%2==0)
+        for (int i = 0; i < 10; i++) {
+            if(i % 2 == 0)
              result = result + i + " ";
         }
         return result;
     }
 
-    private void sayHello(String HELLO) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
-        " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+    private void sayHello(String hello) {
+
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird!"
+                + " Your task is to fix the checkstyle in this file."
+                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println(hello);
     }
 
     private void initializeVariables() {
-        a = "Hello mates!";
-        System.out.println(a);
-        vARIaBLE = LocalDate.now().getYear();
-        System.out.println("It is " + vARIaBLE + "year");
+        message = "Hello mates!";
+        System.out.println(message);
+        year = LocalDate.now().getYear();
+        System.out.println("It is " + year + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
--- src/main/java/core/basesyntax/Model/Dog.java
+++ src/main/java/core/basesyntax/Model/Dog.java
@@ -1,4 +1,16 @@
 package core.basesyntax.Model;
 
 public class Dog {
+    private int age;
+    private String name;
+    private String ownerName;
+
+    @Override
+    public String toString() {
+        return "Dog{"
+                + "age=" + age
+                + ", name='" + name + '\''
+                + ", ownerName='" + ownerName + '\''
+                + '}';
+    }
 }
