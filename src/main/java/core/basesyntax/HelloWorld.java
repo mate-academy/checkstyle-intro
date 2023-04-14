44bb5e5cd930dbd6a55e81d416efc1f3750f5ed8
mariia_mytrofanova <130451095+mary-mi@users.noreply.github.com>
2023-04-14 22:45:14 +0300

    Update HelloWorld.java

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,34 +1,33 @@
 package core.basesyntax;
 
-import core.basesyntax.model.Cat; //rearranged in the correct lexicographical order
+import core.basesyntax.model.Cat;
 import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 // delete unused import
 
-public class HelloWorld { //fixed checkstyle mistakes
+public class HelloWorld { 
     private String text;
-    private int variable;//fixed checkstyle mistakes
+    private int variable;
     private int age;
-    private int size; //initialized the variable on a separate line
+    private int size; 
 
-    //delete empty lines before method
     public static void main(String[] args) {
         Cat cat = new Cat();
         Dog dog = new Dog();
-    } //moved the closing arc to a separate line
+    } 
 
     private String getFromOneToTen() {
-        String result = ""; // add whitespases around =
+        String result = "";
 
-        for (int i = 0; i < 10; i++) { // add whitespases around =, < and before {
-            if (i % 2 == 0) { // add whitespases and {
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
                 result = result + i + " ";
             }
         }
         return result;
     }
 
-    private void sayHello(String hello) { //rename String
+    private void sayHello(String hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! "
                 + "Your task is to fix the checkstyle in this file."
                 + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
