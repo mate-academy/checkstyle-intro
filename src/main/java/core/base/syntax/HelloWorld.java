94311b059e3b84db4c9e1ae215b758ce1853f916
Oleg Stashkiv <stashkivolegg@gmail.com>
2023-06-20 13:12:29 +0300

    Implementation of the proposed changes

--- src/main/java/core/base/syntax/HelloWorld.java
+++ src/main/java/core/base/syntax/HelloWorld.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 
 class HelloWorld {
     private String message;
-    private int variable;
+    private int year;
     private int age;
     private int size;
 
@@ -36,8 +36,8 @@ class HelloWorld {
     private void initializeVariables() {
         message = "Hello mates!";
         System.out.println(message);
-        variable = LocalDate.now().getYear();
-        System.out.println("It is " + variable + "year");
+        year = LocalDate.now().getYear();
+        System.out.println("It is " + year + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
