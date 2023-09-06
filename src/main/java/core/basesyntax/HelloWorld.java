932c8d0e3c01619362662c07a64453dc6ccd9935
barbequeue <p99kab00@gmail.com>
2023-09-06 15:23:48 +0300

    Fix import orders, empty lines, line lenghts, indentations, trailing and missing whitespaces and breaklines

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,34 +1,34 @@
 package core.basesyntax;
 
-import core.basesyntax.model.Dog;
 import core.basesyntax.model.Cat;
-
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
-public class HelloWorld
-{
+public class HelloWorld {
     private String a;
     private int vARIaBLE;
     private int age, size;
 
-
     public static void main(String[] args) {
         Dog dog = new Dog();
-        Cat cat = new Cat(); }
+        Cat cat = new Cat();
+    }
 
     private String getFromOneToTen() {
-        String result="";
-
-        for (int i=0; i<10; i++){
-            if(i%2==0)
-             result = result + i + " ";
+        String result = "";
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                result = result + i + " ";
+            }
         }
         return result;
     }
 
     private void sayHello(String HELLO) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
-        " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird!"
+                            + " Your task is to fix the checkstyle in this file."
+                            + " Your goal is to run `mvn clean package`"
+                            + " and receive `BUILD SUCCESS` message");
         System.out.println(HELLO);
     }
 
