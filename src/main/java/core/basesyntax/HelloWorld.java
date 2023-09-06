51ebb0550f86e9921b1087f365877e5769be042e
barbequeue <p99kab00@gmail.com>
2023-09-06 15:31:31 +0300

    Use StringBuilder in HelloWorld#getFromOneToTen

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -16,13 +16,11 @@ public class HelloWorld {
     }
 
     private String getFromOneToTen() {
-        String result = "";
-        for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0) {
-                result = result + i + " ";
-            }
+        StringBuilder resultString = new StringBuilder();
+        for (int i = 0; i < 10; i += 2) {
+            resultString.append(i).append(' ');
         }
-        return result;
+        return resultString.toString();
     }
 
     private void sayHello(String helloMessage) {
