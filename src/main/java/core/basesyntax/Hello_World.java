8fd3cb494880817ed7b08cfbdd2558b48723e48a
nazariykolesnikoww <nazarkolesnikov536@gmail.com>
2023-09-11 21:57:32 +0300

    Check and update the current code of the Hello_World.java program

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -9,35 +9,33 @@ import java.util.Objects;
 public class Hello_World
 {
     private String a;
-    private int vARIaBLE;
-    private int age, size;
-
+    private int varaible, age, size;
 
     public static void main(String[] args) {
         Dog dog = new Dog();
         Cat cat = new Cat(); }
 
-    private String getFromOneToTen() {
-        String result="";
+   private String getFromOneToTen() {
+        String result = "";
 
-        for (int i=0; i<10; i++){
-            if(i%2==0)
-             result = result + i + " ";
+        for (int i = 0; i <= 10; i++){
+            if(i % 2 == 0)
+             result += i + " ";
         }
         return result;
     }
 
-    private void sayHello(String HELLO) {
+    private void sayHello(String Hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
         " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+        System.out.println(Hello);
     }
 
     private void initializeVariables() {
         a = "Hello mates!";
         System.out.println(a);
-        vARIaBLE = LocalDate.now().getYear();
-        System.out.println("It is " + vARIaBLE + "year");
+        varaible = LocalDate.now().getYear();
+        System.out.println("It is " + varaible + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
