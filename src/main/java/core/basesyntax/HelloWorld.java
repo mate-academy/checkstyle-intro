53a7c910c2c920bc947d5441311eec02c861315f
Nataliia Shtykh <forstudying93@gmail.com>
2023-09-21 09:10:20 +0300

    changed the name of variables

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,8 +5,8 @@ import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-    private String string;
-    private int variable;
+    private String greeting;
+    private int year;
     private int age;
     private int size;
 
@@ -33,10 +33,10 @@ public class HelloWorld {
     }
 
     private void initializeVariables() {
-        string = "Hello mates!";
-        System.out.println(string);
-        variable = LocalDate.now().getYear();
-        System.out.println("It is " + variable + "year");
+        greeting = "Hello mates!";
+        System.out.println(greeting);
+        year = LocalDate.now().getYear();
+        System.out.println("It is " + year + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
