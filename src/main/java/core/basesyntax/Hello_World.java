679a214af28f72ceb19e8f8e55895e3b4d828e5c
Mykyta Nikitin <swifty580073@gmail.com>
2023-09-25 13:24:40 +0300

    More fixing in Hello_World

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -21,7 +21,7 @@ public class Hello_World {
         String result = "";
 
         for (int i = 0; i < 10; i++){
-            if(i % 2 == 0) {
+            if (i % 2 == 0) {
                 result = result + i + " ";
             }
         }
@@ -29,8 +29,8 @@ public class Hello_World {
     }
 
     private void sayHello(String hello) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
-        " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file."
+                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 
