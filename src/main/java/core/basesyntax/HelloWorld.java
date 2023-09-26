fe78bf90abeacc46922396449d1838e904ed28a8
Leonid Bondarenko <leonidbondarenko69@gmail.com>
2023-09-26 16:52:49 +0300

    bugs fixed

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -18,7 +18,7 @@ public class HelloWorld {
     private String getFromOneToTen() {
         String result = "";
         for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0) {
+            if (i% 2 == 0) {
                 result = result + i + " ";
             }
         }
