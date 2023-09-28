18cfb0f0303b3edca240ad8cfe550ecbe5238b2c
Kozharin-Vasul <kozharinvasyl@gmail.com>
2023-09-28 11:41:59 +0300

    Change names of variables

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -4,13 +4,10 @@ import core.basesyntax.Model.Dog;
 import core.basesyntax.Model.Cat;
 
 import java.time.LocalDate;
-import java.util.Objects;
+
 
 public class Hello_World
 {
-    private String a;
-    private int vARIaBLE;
-    private int age, size;
 
 
     public static void main(String[] args) {
@@ -22,22 +19,25 @@ public class Hello_World
 
         for (int i=0; i<10; i++){
             if(i%2==0)
-             result = result + i + " ";
+                result = result + i + " ";
         }
         return result;
     }
 
-    private void sayHello(String HELLO) {
+    private void sayHello(String hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
-        " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+                " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println(hello);
     }
 
     private void initializeVariables() {
+        String a;
+        int variable;
+        int age, size;
         a = "Hello mates!";
         System.out.println(a);
-        vARIaBLE = LocalDate.now().getYear();
-        System.out.println("It is " + vARIaBLE + "year");
+        variable = LocalDate.now().getYear();
+        System.out.println("It is " + variable + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
