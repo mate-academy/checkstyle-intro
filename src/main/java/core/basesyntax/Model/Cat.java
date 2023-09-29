8dc5702b1b83f587dc4fe11905813ebce48a9952
Kozharin-Vasul <kozharinvasyl@gmail.com>
2023-09-29 15:57:24 +0300

    ctrl alt l

--- src/main/java/core/basesyntax/Model/Cat.java
+++ src/main/java/core/basesyntax/Model/Cat.java
@@ -8,10 +8,14 @@ public class Cat {
     @Override
     public String toString() {
         return "Cat{"
-                +"age="
+                + "age="
                 + age
-                +", name='" + name + '\''
-                +", ownerName='" + ownerName + '\''
-                +'}';
+                + ", name='"
+                + name
+                + '\''
+                + ", ownerName='"
+                + ownerName
+                + '\''
+                + '}';
     }
 }
