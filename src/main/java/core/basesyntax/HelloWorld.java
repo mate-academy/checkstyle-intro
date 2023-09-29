fdec9f3ccc7a3f4c11833b0b665566a3d80e843c
Kozharin-Vasul <kozharinvasyl@gmail.com>
2023-09-29 16:50:37 +0300

    ctrl alt l

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import core.basesyntax.model.Cat;
 import core.basesyntax.model.Dog;
-
 import java.time.LocalDate;
 
 public class HelloWorld {
@@ -13,7 +12,7 @@ public class HelloWorld {
 
     private String getFromOneToTen() {
         String result = "";
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
                 result += i
                         + " ";
