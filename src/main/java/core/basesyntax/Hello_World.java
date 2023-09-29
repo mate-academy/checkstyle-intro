fcc4b0788182cdafd5a336e485d8578cc32d3d66
Kozharin-Vasul <kozharinvasyl@gmail.com>
2023-09-29 15:46:51 +0300

    ctrl alt l

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -1,6 +1,8 @@
 package core.basesyntax;
+
 import core.basesyntax.Model.Dog;
 import core.basesyntax.Model.Cat;
+
 import java.time.LocalDate;
 
 public class Hello_World {
@@ -8,6 +10,7 @@ public class Hello_World {
         Dog dog = new Dog();
         Cat cat = new Cat();
     }
+
     private String getFromOneToTen() {
         String result = "";
         for (int i = 0; i < 10; i++) {
@@ -18,12 +21,14 @@ public class Hello_World {
         }
         return result;
     }
+
     private void sayHello(String hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your "
                 + "task is to fix the checkstyle in this file. Your goal"
                 + " is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
+
     private void initializeVariables() {
         String a;
         a = "Hello mates!";
