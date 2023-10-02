c7e7d3f69c593b8012b09ee20f56724a4ada95d0
Mykola Volianskiy <kolavolanskij@gmail.com>
2023-10-02 23:10:11 +0300

    make task autoapprove

---
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -0,0 +1,41 @@
+package core.basesyntax;
+
+import core.basesyntax.Model.Cat;
+import core.basesyntax.Model.Dog;
+import java.time.LocalDate;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+    }
+
+    private String getFromOneToTen() {
+        String result = "";
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                result = result + i + " ";
+            }
+        }
+        return result;
+    }
+
+    private void sayHello(String hello) {
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird!"
+                + " Your task is to fix the checkstyle in this file."
+                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println(hello);
+    }
+
+    private void initializeVariables() {
+        String a = "Hello mates!";
+        System.out.println(a);
+        int variable = LocalDate.now().getYear();
+        System.out.println("It is " + variable + "year");
+        int age = 25;
+        System.out.println("I'm " + age);
+        int size = 42;
+        System.out.println("The size is " + size);
+    }
+}
