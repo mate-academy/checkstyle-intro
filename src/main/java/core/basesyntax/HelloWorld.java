1ccef2eb1a8ec23f0ff92a7555b727cc75fad582
Oleksandr Taranenko <Oleksandrtaranenko239@gmail.com>
2023-10-16 19:37:38 +0300

    fix problem

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,7 +4,7 @@ import core.basesyntax.model.Cat;
 import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
-public class Hello_World {
+public class HelloWorld {
     private String myVariable;
     private int variable;
     private int age;
