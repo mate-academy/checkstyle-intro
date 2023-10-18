77859a7bfb1003360c2b5a1d19d21986a71cd836
Sasha <sasha.panchyshun@gmail.com>
2023-10-18 13:18:41 +0300

    test

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -6,8 +6,7 @@ import core.basesyntax.Model.Cat;
 import java.time.LocalDate;
 import java.util.Objects;
 
-public class Hello_World
-{
+public class Hello_World {
     private String a;
     private int vARIaBLE;
     private int age, size;
@@ -15,21 +14,23 @@ public class Hello_World
 
     public static void main(String[] args) {
         Dog dog = new Dog();
-        Cat cat = new Cat(); }
+        Cat cat = new Cat();
+    }
 
     private String getFromOneToTen() {
-        String result="";
+        String result = "";
 
-        for (int i=0; i<10; i++){
-            if(i%2==0)
-             result = result + i + " ";
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0)
+                result = result + i + " ";
         }
         return result;
     }
 
     private void sayHello(String HELLO) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
-        " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+                " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+
         System.out.println(HELLO);
     }
 
