2f86cf6e4d1c69df36d1a8829e4f5478185d92ad
Orest 40th <student2528@bmet.org.ua>
2023-11-03 23:22:36 +0200

    renamed Model package

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Cat;
-import core.basesyntax.Model.Dog;
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 //import java.util.Objects; unused import
 
