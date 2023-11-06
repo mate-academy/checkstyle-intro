226af67d7844989fcceb95d95b8c25ac10e391ec
Yarolsav <kiritok712@gmail.com>
2023-11-06 10:49:28 +0100

    fix

--- src/main/java/core/basesyntax/Model/Cat.java
+++ src/main/java/core/basesyntax/Model/Cat.java
@@ -8,6 +8,6 @@ public class Cat {
     @Override
     public String toString() {
         return "Cat {" + "age=" + age + ", name = '" + name + '\''
-        + ", ownerName = '" + ownerName + '\'' + '}';
+                + ", ownerName = '" + ownerName + '\'' + '}';
     }
 }
