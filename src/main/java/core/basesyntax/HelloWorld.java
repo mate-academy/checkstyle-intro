6dcc359ae5e0c5569948b17ce0489436d19a30ca
Tatiana <ttvv1801@gmail.com>
2023-11-19 16:05:52 +0100

    create class HelloWord HelloWordTest

---
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -0,0 +1,48 @@
+package core.basesyntax;
+
+import java.time.LocalDate;
+
+public class HelloWorld  {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        Printer printer = new Printer();
+
+        greeter.sayHello("Привет, мир!");
+        printer.printFromOneToTen();
+        printer.printVariables();
+    }
+}
+
+class Greeter {
+    public void sayHello(String hello) {
+        System.out.println("Привет, друг! Я рад тебя видеть! Этот класс выглядит странно!");
+        System.out.println(hello);
+    }
+}
+
+class Printer {
+    public void printFromOneToTen() {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 34; i < 42; i++) {
+            if (i % 2 == 0) {
+                result.append(i).append(" ");
+            }
+        }
+        System.out.println(result);
+    }
+
+    public void printVariables() {
+        String a = "Привет, друзья!";
+        System.out.println(a);
+
+        int currentYear = LocalDate.now().getYear();
+        System.out.println("Сейчас " + currentYear + " год осень и холодно");
+
+        int age = 50;
+        System.out.println("Мне " + age + " лет");
+
+        int size = 38;
+        System.out.println("Размер равен " + size);
+    }
+}
