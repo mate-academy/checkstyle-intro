38fb92d5d53052c5f3c048d2c91b06ef92ec05af
Pavlo Betsa <bigworld2303@gmail.com>
2023-12-01 13:58:00 +0200

    fixed some problems with style

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -6,23 +6,24 @@ import core.basesyntax.Model.Cat;
 import java.time.LocalDate;
 import java.util.Objects;
 
-public class Hello_World
-{
+public class Hello_World {
     private String a;
     private int vARIaBLE;
     private int age, size;
 
 
+
     public static void main(String[] args) {
         Dog dog = new Dog();
-        Cat cat = new Cat(); }
+        Cat cat = new Cat();
+    }
 
     private String getFromOneToTen() {
         String result="";
 
         for (int i=0; i<10; i++){
             if(i%2==0)
-             result = result + i + " ";
+                result = result + i + " ";
         }
         return result;
     }
