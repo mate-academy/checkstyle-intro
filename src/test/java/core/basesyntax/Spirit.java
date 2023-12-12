add28b01793c8e6eb09c17cfd1bc7907835c3a35
Ilia Artemchuk <illaartemhyk@gmail.com>
2023-12-13 00:02:47 +0200

    Створено клас

---
+++ src/test/java/core/basesyntax/Spirit.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+/**
+ * Feel free to remove this class and create your own.
+ */
+public class Spirit extends Home {
+    public void Print (){
+        System.out.println("Hello");
+    }
+}
