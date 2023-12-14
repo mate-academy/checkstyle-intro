c7c995b355d294e56c2e1ce437d92c0c5f182f52
Artem <artem.kozakevych@gmail.com>
2023-12-14 21:51:13 +0200

    checkstyle

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -1,46 +1,41 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Dog;
 import core.basesyntax.Model.Cat;
+import core.basesyntax.Model.Dog;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 public class  Hello_World
 {
-    private String hello;
-    private int year, age, size;
-
-
     public static void main(String[] args) {
         Dog dog = new Dog();
         Cat cat = new Cat();
     }
 
     private String getFromOneToTen() {
-        String result = "";
+        StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < 10; i++) {
             if(i % 2 == 0)
-             result += i + " ";
+             result.append(i).append(" ");
         }
-        return result;
+        return result.toString();
     }
 
-    private void sayHello(String HELLO) {
+    private void sayHello(String hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file."
                 + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+        System.out.println(hello);
     }
 
     private void initializeVariables() {
-        hello = "Hello mates!";
+        String hello = "Hello mates!";
         System.out.println(hello);
-        year = LocalDate.now().getYear();
+        int year = LocalDate.now().getYear();
         System.out.println("It is " + year + "year");
-        age = 25;
+        int age = 25;
         System.out.println("I'm " + age);
-        size = 42;
+        int size = 42;
         System.out.println("The size is " + size);
     }
 }
