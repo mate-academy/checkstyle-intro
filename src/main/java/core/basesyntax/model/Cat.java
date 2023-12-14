f857c5dc84b67c0b7b62a53477668cd4465c379a
Artem <artem.kozakevych@gmail.com>
2023-12-14 22:40:06 +0200

    fix style in cat class

--- src/main/java/core/basesyntax/model/Cat.java
+++ src/main/java/core/basesyntax/model/Cat.java
@@ -8,9 +8,14 @@ public class Cat {
     @Override
     public String toString() {
         return "Cat{" +
-                "age=" + age +
-                ", name='" + name + '\'' +
-                ", ownerName='" + ownerName + '\'' +
-                '}';
+                "age="
+                + age
+                + ", name='"
+                + name
+                + '\''
+                + ", ownerName='"
+                + ownerName
+                + '\''
+                + '}';
     }
 }
