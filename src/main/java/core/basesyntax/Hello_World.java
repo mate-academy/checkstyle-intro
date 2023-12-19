a4594d2019f12a83f28d86424d20d75e234372c3
Vlad Shenderchuk <shenderchuk.vlad@gmail.com>
2023-12-19 20:35:43 +0200

    change getFromOneToTen

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -1,30 +1,27 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Dog;
-import core.basesyntax.Model.Cat;
-
 import java.time.LocalDate;
-import java.util.Objects;
 
 public class Hello_World
 {
-    private String a;
-    private int vARIaBLE;
-    private int age, size;
+    public String a;
+    public int vARIaBLE;
+    public int age, size;
+
 
 
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        Cat cat = new Cat(); }
 
-    private String getFromOneToTen() {
-        String result="";
+    }
+
+    public String getFromOneToTen() {
+        StringBuilder result= new StringBuilder();
 
         for (int i=0; i<10; i++){
             if(i%2==0)
-             result = result + i + " ";
+             result.append(i).append(" ");
         }
-        return result;
+        return result.toString();
     }
 
     private void sayHello(String HELLO) {
