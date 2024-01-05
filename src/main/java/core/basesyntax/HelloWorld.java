4f678e2edb9ac931d71154b89b6873248949a5ba
Bohdan <bohdan2704@gmail.com>
2024-01-05 22:16:19 +0200

    reformating second time

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -8,8 +8,8 @@ import java.time.LocalDate;
 public class HelloWorld {
     private String a;
     private int variable;
-    private int age, size;
-
+    private int age;
+    private int size;
 
     public static void main(String[] args) {
         Dog dog = new Dog();
@@ -20,16 +20,18 @@ public class HelloWorld {
         StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0) result.append(i).append(" ");
+            if (i % 2 == 0) {
+                result.append(i).append(" ");
+            }
         }
         return result.toString();
     }
 
-    private void sayHello(String HELLO) {
+    private void sayHello(String hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task "
                 + "is to fix the checkstyle in this file. Your goal is to run `mvn clean "
                 + "package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+        System.out.println(hello);
     }
 
     private void initializeVariables() {
