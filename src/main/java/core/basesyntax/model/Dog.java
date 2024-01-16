38bb3d22a47ab8b5865dbb6c02d17916e61226fd
gulini32 <gulini32@gmail.com>
2024-01-16 22:33:33 +0200

    HW

--- src/main/java/core/basesyntax/model/Dog.java
+++ src/main/java/core/basesyntax/model/Dog.java
@@ -1,4 +1,20 @@
 package core.basesyntax.model;
 
 public class Dog {
+    private int age;
+    private String name;
+    private String ownerName;
+
+    @Override
+    public String toString() {
+        return "Dog{"
+                +
+                "age=" + age
+                +
+                ", name='" + name + '\''
+                +
+                ", ownerName='" + ownerName + '\''
+                +
+                '}';
+    }
 }
