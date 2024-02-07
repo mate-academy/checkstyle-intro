c7624fcc8ed33a111a808c6baa08ca41aa1752b3
Denys <Eidenn005@gmail.com>
2024-02-07 22:45:43 +0200

    Fixin after maven report

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,16 +4,11 @@ import core.basesyntax.model.Cat;
 import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
-public class Hello_World {
-    private String a;
-    private int variable;
-    private int age, size;
-
-
+public class HelloWorld {
     public static void main(String[] args) {
         Dog dog = new Dog();
         Cat cat = new Cat();
-        Hello_World hw = new Hello_World();
+        HelloWorld hw = new HelloWorld();
         hw.sayHello("Hey");
         hw.getFromOneToTen();
         hw.initializeVariables();
@@ -29,20 +24,21 @@ public class Hello_World {
         return result.toString();
     }
 
-    private void sayHello(String HELLO) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file."
+    private void sayHello(String hello) {
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! "
+                + "Your task is to fix the checkstyle in this file."
                 + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+        System.out.println(hello);
     }
 
     private void initializeVariables() {
-        a = "Hello mates!";
+        String a = "Hello mates!";
         System.out.println(a);
-        variable = LocalDate.now().getYear();
+        int variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + "year");
-        age = 25;
+        int age = 25;
         System.out.println("I'm " + age);
-        size = 42;
+        int size = 42;
         System.out.println("The size is " + size);
     }
 }
