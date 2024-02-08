4df5eeb0b61c9e60d0b1df7da5a5b46cc3778492
Vlad Poyda <playeeer.steam@gmail.com>
2024-02-08 17:05:38 +0200

    Fix some bags

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -30,7 +30,7 @@ public class HelloWorld {
         System.out.println(
                 "Hello, Mate! I am happy to see you! This class looks weird! "
                         + "Your task is to fix the checkstyle in this file."
-                    + " Your goal is to run `mvn clean package` and receive "
+                        + " Your goal is to run `mvn clean package` and receive "
                         + "`BUILD SUCCESS` message");
         System.out.println(hello);
     }
