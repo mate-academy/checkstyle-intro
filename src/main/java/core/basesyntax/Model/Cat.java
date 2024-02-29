cbfc3c33885fa7a013b46aa531f02ceb705a2fdb
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-02-29 08:42:53 +0200

    Cat.java

--- src/main/java/core/basesyntax/Model/Cat.java
+++ src/main/java/core/basesyntax/Model/Cat.java
@@ -4,10 +4,11 @@ public class Cat {
     private String name;
     private int age;
 
-   @Override
-   public String toString() {
-    return "Cat{" 
-        + "name='" + name + '\'' 
-        + ", age=" + age 
-        + '}';
+    @Override
+    public String toString() {
+        return "Cat{" 
+            + "name='" + name + '\'' 
+            + ", age=" + age 
+            + '}';
+    }
 }
