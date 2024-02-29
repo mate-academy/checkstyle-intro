1189e36b754da21d6367679c2e08a8336b192a18
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-02-29 08:32:44 +0200

    Cat.java

--- src/main/java/core/basesyntax/Model/Cat.java
+++ src/main/java/core/basesyntax/Model/Cat.java
@@ -1,5 +1,38 @@
 package core.basesyntax.model;
 
-public class Dog {
-    // Реалізація класу Dog
+public class Cat {
+    private String name;
+    private int age;
+
+    public Cat() {
+    }
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
