54a8134a4ed9f3fb6182c2185db505ac2d9bdc5d
Anastasiia Solenova <anastasiiasolenova@gmail.com>
2024-03-11 02:14:33 +0200

    changes were made

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,18 +4,18 @@ import core.basesyntax.model.Cat;
 import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
-public class Hello_world {
+public class HelloWorld {
     private String hello;
-    private String a;
+
+    private String someVariable;
     private int variable;
     private int age;
     private int size;
 
-
     public static void main(String[] args) {
-    Dog dog = new Dog();
+        Dog dog = new Dog();
         Cat cat = new Cat();
-        }
+    }
 
     private String getFromOneToTen() {
         String result = "";
@@ -23,18 +23,19 @@ public class Hello_world {
         for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
                 result = result + i + " ";
+            }
         }
         return result;
     }
 
     private void sayHello(String hello) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
-        " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println("Hello, Mate! I am happy to see you!"
+                + "Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 
     private void initializeVariables() {
-        a = "Hello mates!";
+        String a = "Hello mates!";
         System.out.println(a);
         variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + "year");
@@ -44,7 +45,3 @@ public class Hello_world {
         System.out.println("The size is " + size);
     }
 }
-
-    public void setHello(String hello) {
-        Hello = hello;
-    }
