4cb9e7b23625a54e0239295f8a129533ac89a07f
Yura Yurchuk <yurayurchuk88@gmail.com>
2024-03-27 22:08:24 +0200

    fix all code  style

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,21 +1,19 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Dog;
 import core.basesyntax.Model.Cat;
-
+import core.basesyntax.Model.Dog;
 import java.time.LocalDate;
-import java.util.Objects;
 
-public class Hello_World
-{
+public class Hello_World {
     private String a;
-    private int vARIaBLE;
-    private int age, size;
-
+    private int variable;
+    private int age;
+    private int size;
 
     public static void main(String[] args) {
         Dog dog = new Dog();
-        Cat cat = new Cat(); }
+        Cat cat = new Cat();
+    }
 
     private String getFromOneToTen() {
         String result="";
@@ -36,8 +34,8 @@ public class Hello_World
     private void initializeVariables() {
         a = "Hello mates!";
         System.out.println(a);
-        vARIaBLE = LocalDate.now().getYear();
-        System.out.println("It is " + vARIaBLE + "year");
+        variable = LocalDate.now().getYear();
+        System.out.println("It is " + variable + "year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
