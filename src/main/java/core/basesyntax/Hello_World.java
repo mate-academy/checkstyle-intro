3e901618076763f72bab4a77a4fbce0a9d3c23f9
Polina Kirf <polinakirf06@gmail.com>
2024-03-29 15:04:10 +0300

    fix code style

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -1,46 +1,76 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Dog;
 import core.basesyntax.Model.Cat;
+import core.basesyntax.Model.Dog;
 
 import java.time.LocalDate;
-import java.util.Objects;
+import java.util.Arrays;
 
-public class Hello_World
-{
+public static class Hello_World {
     private String a;
     private int vARIaBLE;
     private int age, size;
+    public String getA() {
+        return a;
+    }
+    public void setA(String a) {
+        this.a = a;
+    }
+    public int getvARIaBLE(){
+        return vARIaBLE;
+    }
+    public void setvARIaBLE(int vARIaBLE){
+        this.vARIaBLE = vARIaBLE;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getSize(){
+        return size;
+    }
+    public void setSize(int size) {
+        this.size = size;
+    }
 
+}
 
     public static void main(String[] args) {
         Dog dog = new Dog();
-        Cat cat = new Cat(); }
-
-    private String getFromOneToTen() {
-        String result="";
-
-        for (int i=0; i<10; i++){
-            if(i%2==0)
-             result = result + i + " ";
-        }
-        return result;
-    }
-
-    private void sayHello(String HELLO) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
-        " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
-    }
-
-    private void initializeVariables() {
-        a = "Hello mates!";
-        System.out.println(a);
-        vARIaBLE = LocalDate.now().getYear();
-        System.out.println("It is " + vARIaBLE + "year");
-        age = 25;
-        System.out.println("I'm " + age);
-        size = 42;
-        System.out.println("The size is " + size);
-    }
+        Cat cat = new Cat();
+System.out.println(dog);
+System.out.println(cat);
 }
+    public static String getFromOneToTen() {
+        StringBuilder result= new StringBuilder();
+
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                result = new StringBuilder(i + result.toString() + ' ');
+            }
+        }
+        return result.toString();
+    }
+
+
+public static void sayHello(String HELLO) {
+        for (String s : Arrays.asList("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
+                " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message", HELLO)) {
+            System.out.println(s);
+        }
+    }
+
+    public static void initializeVariables() {
+        String a = "Hello mates!";
+        System.out.println(a);
+        int vARIaBLE = LocalDate.now().getYear();
+        System.out.println(STR."It is\{vARIaBLE}year");
+        int age = 25;
+        System.out.println(STR."I'm \{age}");
+        int size = 42;
+        System.out.println(STR."The size is\{size}");
+    }
+
+
