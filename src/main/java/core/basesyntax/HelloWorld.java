3268a4c0013db221e639b701e17bec8a07a74209
ttikr <ia03.danchuk.volodymyr@gmail.com>
2024-07-08 19:22:49 +0300

    fixed test failure

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -17,8 +17,6 @@ public class HelloWorld {
 
     private String getFromOneToTen() {
         String result = "";
-
-
         for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
                 result += i + " ";
