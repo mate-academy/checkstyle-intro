3869caaf67d1a6172e2c07faa83a576db56774a2
Alinka Lyn <alinka.lyn@ukr.net>
2024-07-23 16:11:14 +0300

    added fixes after review

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -6,9 +6,7 @@ import java.time.LocalDate;
 
 public class HelloWorld {
 
-
     public static void main(String[] args) {
-
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.sayHello();
         System.out.println(helloWorld.getFromOneToTen());
@@ -16,7 +14,7 @@ public class HelloWorld {
     }
 
     private String getFromOneToTen() {
-        StringBuilder result=new StringBuilder();
+        StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
@@ -28,18 +26,18 @@ public class HelloWorld {
 
     private void sayHello() {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
-        " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+                " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println("Hello from main!");
     }
 
     private void initializeVariables() {
         String a = "Hello mates!";
         System.out.println(a);
-         int variable = LocalDate.now().getYear();
-        System.out.println("It is " + variable + "year");
+        int variable = LocalDate.now().getYear();
+        System.out.println("It is " + variable + " year");
         int age = 25;
         System.out.println("I'm " + age);
-       int size = 42;
+        int size = 42;
         System.out.println("The size is " + size);
     }
 }
