e4352cb7391bf4f07fd4e33400656fe738ee94c5
Serhii Holenkov <holenkov.s.s@gmail.com>
2024-08-04 16:53:17 +0300

    solved errors

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,7 +5,7 @@ import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-    private String aword;
+    private String aword1;
     private int variable;
     private int age;
     private int size;
@@ -34,8 +34,8 @@ public class HelloWorld {
     }
 
     private void initializeVariables() {
-        aword = "Hello mates!";
-        System.out.println(aword);
+        aword1 = "Hello mates!";
+        System.out.println(aword1);
         variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + " year");
         age = 25;
