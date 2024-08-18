cdad0ee71ca3ab50400adfad9b65ad3105ec822e
Alinka Lyn <alinka.lyn@ukr.net>
2024-08-18 17:38:49 +0300

    change after review

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
-import core.basesyntax.model.Dog;
 import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 
 import java.time.LocalDate;
 
@@ -10,7 +10,6 @@ public class HelloWorld {
     private int variable;
     private int age;
     private int size;
-
 
     public static void main(String[] args) {
         Dog dog = new Dog();
@@ -29,8 +28,9 @@ public class HelloWorld {
     }
 
     private void sayHello(String hello) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
-                " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird!"
+                + " Your task is to fix the checkstyle in this file."
+                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 
