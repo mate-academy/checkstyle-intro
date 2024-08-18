973d3633881a126e69a72701c2cf9fecf1b5877f
Alinka Lyn <alinka.lyn@ukr.net>
2024-08-18 17:26:27 +0300

    fixed all problems

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,8 +5,7 @@ import core.basesyntax.Model.Cat;
 
 import java.time.LocalDate;
 
-public class HelloWorld
-{
+public class HelloWorld {
     private String message;
     private int variable;
     private int age;
@@ -15,28 +14,30 @@ public class HelloWorld
 
     public static void main(String[] args) {
         Dog dog = new Dog();
-        Cat cat = new Cat(); }
+        Cat cat = new Cat();
+    }
 
     private String getFromOneToTen() {
-        String result="";
+        String result = "";
 
-        for (int i=0; i<10; i++){
-            if(i%2==0)
-             result = result + i + " ";
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                result = result + i + " ";
+            }
         }
         return result;
     }
 
     private void sayHello(String hello) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
-        " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+                " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 
     private void initializeVariables() {
         message = "Hello mates!";
         System.out.println(message);
-        variable= LocalDate.now().getYear();
+        variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + "year");
         age = 25;
         System.out.println("I'm " + age);
--- src/main/java/core/basesyntax/Model/Cat.java
+++ src/main/java/core/basesyntax/Model/Cat.java
@@ -7,10 +7,15 @@ public class Cat {
 
     @Override
     public String toString() {
-        return "Cat{" +
-                "age=" + age +
-                ", name='" + name + '\'' +
-                ", ownerName='" + ownerName + '\'' +
-                '}';
+        return "Cat{"
+                + "age="
+                + age
+                + ", name='"
+                + name
+                + '\''
+                + ", ownerName='"
+                + ownerName
+                + '\''
+                + '}';
     }
 }
