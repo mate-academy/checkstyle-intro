b30b38088dd95d96b86213fed9db946a3c92e743
Kate Kraska <kate2002.kk42@gmail.com>
2024-09-23 14:44:43 +0300

    Initial commit for hw-solution

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,12 +1,11 @@
 package core.basesyntax;
 
-import core.basesyntax.model.Dog;
 import core.basesyntax.model.Cat;
-
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-    private String a;
+    private String variableName;
     private int variable;
     private int age;
     private int size;
@@ -19,7 +18,7 @@ public class HelloWorld {
     private String getFromOneToTen() {
         String result = "";
         for (int i = 0; i < 10; i++) {
-            if(i % 2 == 0) {
+            if (i % 2 == 0) {
                 result = result + i + " ";
             }
         }
@@ -27,15 +26,15 @@ public class HelloWorld {
     }
 
     private void sayHello(String hello) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! " +
-                "Your task is to fix the checkstyle in this file." +
-        " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! "
+                + "Your task is to fix the checkstyle in this file."
+                 + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 
     private void initializeVariables() {
-        a = "Hello mates!";
-        System.out.println(a);
+        variableName = "Hello mates!";
+        System.out.println(variableName);
         variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + "year");
         age = 25;
