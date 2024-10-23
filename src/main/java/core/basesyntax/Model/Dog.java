5a57c0b5c1fc2acbf6e6cdf456ce55ae1be05930
MaksymMysiuk <maks140806@gmail.com>
2024-10-23 15:36:05 +0200

    update

--- src/main/java/core/basesyntax/Model/Dog.java
+++ src/main/java/core/basesyntax/Model/Dog.java
@@ -1,4 +1,16 @@
-package core.basesyntax.Model;
+package core.basesyntax.model;
 
 public class Dog {
+    private int age;
+    private String name;
+    private String breed;
+
+    @Override
+    public String toString() {
+        return "Dog{"
+                + "age=" + age
+                + ", name='" + name + '\''
+                + ", breed='" + breed + '\''
+                + '}';
+    }
 }
--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -3,6 +3,21 @@ package core.basesyntax;
 /**
  * Feel free to remove this class and create your own.
  */
-public class HelloWorldTest {
+public class package core.basesyntax.model;
+
+public class Dog {
+    private int age;
+    private String name;
+    private String breed;
+
+    @Override
+    public String toString() {
+        return "Dog{"
+                + "age=" + age
+                + ", name='" + name + '\''
+                + ", breed='" + breed + '\''
+                + '}';
+    }
+}HelloWorldTest {
 
 }
