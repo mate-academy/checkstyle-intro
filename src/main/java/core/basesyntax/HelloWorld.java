24599de1fa30faa1585daad293c02b9c8929073f
Teslenko Daniil <burgalhashem@gmail.com>
2024-10-29 12:46:49 +0200

    fixed checkstyle

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,16 +1,15 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Dog;
-import core.basesyntax.Model.Cat;
-
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
-import java.util.Objects;
+//import java.util.Objects;
 
-public class Hello_World
-{
+public class HelloWorld {
     private String a;
     private int variable;
-    private int age, size;
+    private int age;
+    private int size;
 
     public static void main(String[] args) {
         Dog dog = new Dog();
@@ -18,18 +17,20 @@ public class Hello_World
     }
 
     private String getFromOneToTen() {
-        String result="";
-        for (int i = 0; i < 10; i++){
-            if (i % 2 == 0)
-             result = result + i + " ";
+        String result = "";
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                result = result + i + " ";
+            }
         }
         return result;
     }
 
-    private void sayHello(String HELLO) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file."
+    private void sayHello(String Hello) {
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! "
+                + "Your task is to fix the checkstyle in this file."
                 + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+        System.out.println(Hello);
     }
 
     private void initializeVariables() {
