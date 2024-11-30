ff26729a27120141bb594d7b8cae4a21696dfc47
Daniil Sitailo <serzsito2709@gmail.com>
2024-11-30 17:02:48 +0200

    Add psvm and print Hello World!

--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -1,8 +1,7 @@
 package core.basesyntax;
-
-/**
- * Feel free to remove this class and create your own.
- */
 public class HelloWorldTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 
 }
