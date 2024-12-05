df44e34db570f130f2257ce0af956210c6a62f89
dzxd16 <dzxd16@gmail.com>
2024-12-05 21:04:07 +0200

    second finish correct mistakes

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         sayHello(getFromOneToTen());
     }
+
     private static String getFromOneToTen() {
         StringBuilder result = new StringBuilder();
 
@@ -20,15 +21,16 @@ public class HelloWorld {
                 "Hello, Mate! I am happy to see you!"
         );
         System.out.println(
-                        "This class looks weird!"
+                "This class looks weird!"
         );
         System.out.println(
-                        "Your task is to fix the checkstyle in this file."
-        );        System.out.println(
-                        "Your goal is to run `mvn clean package`"
+                 "Your task is to fix the checkstyle in this file."
         );
         System.out.println(
-                        "and receive `BUILD SUCCESS` message."
+                  "Your goal is to run `mvn clean package`"
+        );
+        System.out.println(
+                  "and receive `BUILD SUCCESS` message."
         );
         System.out.println(hello);
     }
--- src/main/java/core/basesyntax/model/Cat.java
+++ src/main/java/core/basesyntax/model/Cat.java
@@ -4,6 +4,7 @@ public class Cat {
     private int age;
     private String name;
     private String ownerName;
+
     @Override
     public String toString() {
         return "Cat { "
