f03aec4c04f1340de94b1be1e4b32c7a9152fdb7
dzxd16 <dzxd16@gmail.com>
2024-12-05 18:55:31 +0200

    Refactoring

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -1,46 +1,24 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Dog;
-import core.basesyntax.Model.Cat;
-
-import java.time.LocalDate;
-import java.util.Objects;
-
-public class Hello_World
-{
-    private String a;
-    private int vARIaBLE;
-    private int age, size;
-
-
+public class Hello_World {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        Cat cat = new Cat(); }
-
-    private String getFromOneToTen() {
-        String result="";
-
-        for (int i=0; i<10; i++){
-            if(i%2==0)
-             result = result + i + " ";
-        }
-        return result;
+        sayHello(getFromOneToTen());
     }
 
-    private void sayHello(String HELLO) {
+    private static String getFromOneToTen() {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0)
+                result.append(i).append(" ");
+        }
+        return result.toString();
+    }
+
+    private static void sayHello(String HELLO) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
-        " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+                " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(HELLO);
     }
 
-    private void initializeVariables() {
-        a = "Hello mates!";
-        System.out.println(a);
-        vARIaBLE = LocalDate.now().getYear();
-        System.out.println("It is " + vARIaBLE + "year");
-        age = 25;
-        System.out.println("I'm " + age);
-        size = 42;
-        System.out.println("The size is " + size);
-    }
 }
