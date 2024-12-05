bc6a5135854e63f1f5805d7f6548253d1ed5cb1a
dzxd16 <dzxd16@gmail.com>
2024-12-05 19:09:03 +0200

    correct mistakes

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class Hello_World {
+public class HelloWorld {
     public static void main(String[] args) {
         sayHello(getFromOneToTen());
     }
@@ -9,16 +9,18 @@ public class Hello_World {
         StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0)
+            if (i % 2 == 0) {
                 result.append(i).append(" ");
+            }
         }
         return result.toString();
     }
 
-    private static void sayHello(String HELLO) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
+    private static void sayHello(String hello) {
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task " +
+                "is to fix the checkstyle in this file." +
                 " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+        System.out.println(hello);
     }
 
 }
