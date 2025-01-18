0abee7a74caf2806339eb7a0eb6bbdb2e7e4794a
Merquer <rostislav1am@gmail.com>
2025-01-18 14:13:49 +0200

    fixed checkstyle errors v2

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,18 +1,17 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Cat;
-import core.basesyntax.Model.Dog;
-
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-    private String a;
+    private String peopleGraatings;
     private int age;
     private int variable;
     private int size;
 
-
     public static void main(String[] args) {
+
         Cat cat = new Cat();
         Dog dog = new Dog();
     }
@@ -30,16 +29,16 @@ public class HelloWorld {
 
     private void sayHello(String hello) {
         System.out.println("Hello, Mate! I am happy to see you! "
-        + "This class looks weird! Your task is to fix the checkstyle in this file."
-        + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+                + "This class looks weird! Your task is to fix the checkstyle in this file."
+                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
         System.out.println(hello);
     }
 
     private void initializeVariables() {
-        a = "Hello mates!";
+        peopleGraatings = "Hello mates!";
         age = 25;
         size = 42;
-        System.out.println(a);
+        System.out.println(peopleGraatings);
         variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + "year");
         System.out.println("I'm " + age);
