11e0d1fc629c3ee1c9d5b2bf71855754ac6f4834
Yuri Fedorov <fedorov.ovk@gmail.com>
2025-01-27 20:44:42 +0200

    Train push code on github

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -5,7 +5,7 @@ import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
-    private String argum;
+    private String arg;
     private int variable;
     private int age;
     private int size;
@@ -34,8 +34,8 @@ public class HelloWorld {
     }
 
     private void initializeVariables() {
-        argum = "Hello mates!";
-        System.out.println(argum);
+        arg = "Hello mates!";
+        System.out.println(arg);
         variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + "year");
         age = 25;
