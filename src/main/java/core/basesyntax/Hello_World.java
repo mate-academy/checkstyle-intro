d0902466f02f78d0452ce21765c4ca65bf65d463
anton <anton.surzhok@gmail.com>
2025-01-27 15:35:33 +0200

    test

--- src/main/java/core/basesyntax/Hello_World.java
+++ src/main/java/core/basesyntax/Hello_World.java
@@ -1,43 +1,42 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Dog;
-import core.basesyntax.Model.Cat;
+import core.basesyntax.model.Dog;
+import core.basesyntax.model.Cat;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
-public class Hello_World
-{
-    private String a;
-    private int vARIaBLE;
-    private int age, size;
-
+class HelloWorld {
+    private String message;
+    private int year;
+    private int age;
+    private int size;
 
     public static void main(String[] args) {
         Dog dog = new Dog();
-        Cat cat = new Cat(); }
-
-    private String getFromOneToTen() {
-        String result="";
-
-        for (int i=0; i<10; i++){
-            if(i%2==0)
-             result = result + i + " ";
-        }
-        return result;
+        Cat cat = new Cat();
     }
 
-    private void sayHello(String HELLO) {
+    private String getEvenNumbersFromOneToTen() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                result.append(i).append(" ");
+            }
+        }
+        return result.toString();
+    }
+
+    private void sayHello(String greeting) {
         System.out.println("Hello, Mate! I am happy to see you! This class looks weird! Your task is to fix the checkstyle in this file." +
-        " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(HELLO);
+                " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message.");
+        System.out.println(greeting);
     }
 
     private void initializeVariables() {
-        a = "Hello mates!";
-        System.out.println(a);
-        vARIaBLE = LocalDate.now().getYear();
-        System.out.println("It is " + vARIaBLE + "year");
+        message = "Hello mates!";
+        System.out.println(message);
+        year = LocalDate.now().getYear();
+        System.out.println("It is " + year + " year");
         age = 25;
         System.out.println("I'm " + age);
         size = 42;
---
+++ src/main/java/core/basesyntax/model/Dog.java
@@ -0,0 +1,40 @@
+package core.basesyntax.model;
+
+public class Dog {
+    private int age;
+    private String breed;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "age=" + age +
+                ", breed='" + breed + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
